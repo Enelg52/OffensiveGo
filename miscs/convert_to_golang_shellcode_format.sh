1964eb75232f7033735d9ccfa3c82ab69b3cc0c1
RistBS <75935486+RistBS@users.noreply.github.com>
2023-04-01 21:48:28 +0200

    Create convert_to_golang_shellcode_format.sh

---
+++ miscs/convert_to_golang_shellcode_format.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#
+# Converts a Cobalt Strike shellcode from C format to Golang format
+#
+#
+
+# take "payload.c" shellcode beacon 
+beacon_file="$1"
+
+beacon_file_contents=$(<"$beacon_file")
+
+go_shellcode=$( echo "$beacon_file_contents" | sed 's/unsigned char buf\[\] = "\(.*\)"/buf = []byte{ \1 }/' )
+go_shellcode=$( echo "$go_shellcode"         | sed 's/\\x\([0-9A-Fa-f][0-9A-Fa-f]\)/0x\1, /g' ) 
+
+echo "$go_shellcode"
+
