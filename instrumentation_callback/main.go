6eb4f849214a3d6fe2c271e0bc0eaf10b76ae47d
RistBS <75935486+RistBS@users.noreply.github.com>
2023-04-06 23:06:45 +0200

    Create main.go

---
+++ instrumentation_callback/main.go
@@ -0,0 +1,34 @@
+package main
+
+
+import (
+	"unsafe"
+	"syscall"
+)
+
+
+const (
+	ProcessInstrumentationCallback = 40
+)
+
+type PROCESS_INSTRUMENTATION_CALLBACK_INFORMATION struct {
+	Version  uint32
+	Reserved uint32
+	Callback uintptr
+}
+
+
+func main() {
+	NtSetInformationProcess := syscall.NewLazyDLL("ntdll").NewProc("NtSetInformationProcess")
+
+	var InstrumentationCallbackInfo PROCESS_INSTRUMENTATION_CALLBACK_INFORMATION
+
+	InstrumentationCallbackInfo.Version  = 0x0
+	InstrumentationCallbackInfo.Reserved = 0x0
+	InstrumentationCallbackInfo.Callback = 0x0
+
+	NtSetInformationProcess.Call( uintptr( 0xffffffffffffffff ),
+								  ProcessInstrumentationCallback,
+	  							  uintptr( unsafe.Pointer( &InstrumentationCallbackInfo ) ),
+								  unsafe.Sizeof( InstrumentationCallbackInfo ) )
+}
